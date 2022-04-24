0670d589748fdb07b38b10fcc8f0973ecbb16a0c
Albert <adbforlife2018@gmail.com>
2022-04-24 08:54:26 -0400

    Set up

--- dpf-impl/dpf-tree.cpp
+++ dpf-impl/dpf-tree.cpp
@@ -12,12 +12,29 @@ DpfTree::DpfTree(int p, int m, int lambda) : p_(p), m_(m) {
 }
 
 bits DpfTree::Gen(bits ind) {
-  // Line 2: Generate seeds
+  // 1: Generate seeds
   std::vector<Seed> ss;
   for (int i = 0; i < p_; ++i) {
     ss.push_back({});
     gen_key(ss[i].s);
   }
+
+  // 2: Initialize control bits
+  std::vector<std::vector<uint8_t>> ts;
+  for (int j = 1; j < p_+1; ++j) {
+    ts.push_back({});
+    for (int k = 2; k < p_+1; ++k) {
+      if (k == j) ts[j].push_back(1);
+      else ts[j].push_back(0);
+    }
+  }
+
+  // 3. Starting loop for each index bit
+  for (int i = 1; i < m_+1; ++i) {
+    continue;
+  }
+
+  
   return {0,1};
 }
 
